28cc19dea4557a1fbc9b56a83e0ccfa06f7d89f5
Kakise <sam.taa@icloud.com>
2020-09-16 12:47:17 +0200

    Added blog files

---
+++ src/Blog/BlogHome.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom'
+import Butter from 'buttercms'
+
+const butter = Butter('1f984113d19d94aeba9f2a731197b9993b18a369');
+
+class BlogHome extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loaded: false
+        };
+    }
+
+    fetchPosts(page) {
+        butter.post.list({page: page, page_size: 10}).then((resp) => {
+            this.setState({
+                loaded: true,
+                resp: resp.data
+            })
+        });
+    }
+
+    componentDidMount() {
+        const page = this.props.match.params.page;
+
+        this.fetchPosts(page)
+    }
+
+    render() {
+        if (this.state.loaded) {
+            const { next_page, previous_page } = this.state.resp.meta;
+
+            return (
+                <div>
+                    {this.state.resp.data.map((post) => {
+                        return (
+                            <div className="post-link" key={post.slug}>
+                                <Link to={`/post/${post.slug}`}>{post.title}</Link>
+                            </div>
+                        )
+                    })}
+
+                    <br />
+
+                    <div>
+                        {previous_page && <Link to={`/p/${previous_page}`}>Prev</Link>}
+
+                        {next_page && <Link to={`/p/${next_page}`}>Next</Link>}
+                    </div>
+                </div>
+            );
+        } else {
+            return (
+                <div className="Loading">
+                    Loading...
+                </div>
+            )
+        }
+    }
+}
+
+export default withRouter(BlogHome);
---
+++ src/Blog/BlogPost.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import Butter from 'buttercms'
+import { Helmet } from "react-helmet";
+import { withRouter } from 'react-router-dom';
+
+const butter = Butter('1f984113d19d94aeba9f2a731197b9993b18a369');
+
+class BlogPost extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loaded: false
+        };
+    }
+
+    componentDidMount() {
+        const slug = this.props.match.params.slug;
+
+        butter.post.retrieve(slug).then((resp) => {
+            this.setState({
+                loaded: true,
+                post: resp.data.data
+            })
+        });
+    }
+
+    render() {
+        if (this.state.loaded) {
+            const post = this.state.post;
+
+            return (
+                <div>
+                    <Helmet>
+                        <title>{post.seo_title}</title>
+                        <meta name="description" content={post.meta_description} />
+                        <meta name="og:image" content={post.featured_image} />
+                    </Helmet>
+
+                    <h1>{post.title}</h1>
+                    <div dangerouslySetInnerHTML={{__html: post.body}} />
+                </div>
+            );
+        } else {
+            return (
+                <div className="loading">
+                    Loading...
+                </div>
+            );
+        }
+    }
+}
+
+export default withRouter(BlogPost);
