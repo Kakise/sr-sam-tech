221084e10903c3522bf895d3ee35a4b36cc7dfb8
Kakise <sam.taa@icloud.com>
2020-10-20 17:40:04 +0200

    Added cache expiration

--- src/Blog/Categories.js
+++ src/Blog/Categories.js
@@ -8,49 +8,80 @@ const Header = React.lazy(() => import('./partial/Header'));
 const Sidebar = React.lazy(() => import('./partial/Sidebar'));
 const butter = Butter('1f984113d19d94aeba9f2a731197b9993b18a369');
 
+function loadingDiv() {
+    return (
+        <div className="loading">
+            Loading...
+        </div>
+    );
+}
 
 class Categories extends Component {
-    state = {
-        data: []
-    }
+    constructor(props) {
+        super(props);
 
-    async componentDidMount() {
-        const cache = JSON.parse(sessionStorage.getItem("categories"));
+        const cache = JSON.parse(localStorage.getItem("categories"));
 
         if (!cache) {
-            const resp = await butter.category.list()
-            this.setState(resp.data);
-            sessionStorage.setItem("categories", JSON.stringify(this.state.data));
-        } else {
-            this.setState({data: cache});
+            this.state = {
+                loaded: false
+            };
+        } else if (Date.now() - cache.retrieved < 86400000) {
+            this.state = {
+                loaded: true,
+                resp: cache,
+            };
             console.log("Page loaded from cache");
+            console.log("Page is " + (Date.now() - this.state.resp.retrieved).toString() + " ms old");
+        } else {
+            this.state = {
+                loaded: false,
+            };
         }
 
     }
-    render () {
-        return (
-            <div className="grid">
-                <Helmet>
-                    <title>Sam's TechBlog - Catégories</title>
-                </Helmet>
-                <Suspense fallback={<div className="loading">Loading...</div>}>
-                    <Header />
-                    <Sidebar />
-                </Suspense>
-                <div className="categories">
-                    <h1>Liste des catégories</h1>
-                    {this.state.data.map((category, key) => {
-                        return (
-                            <Link to={`/blog/category/${category.slug}`}>
-                                <div className="box" key={key}>
-                                    <span>{category.name}</span>
-                                </div>
-                            </Link>
-                        )
-                    })}
+
+    async componentDidMount() {
+        if (!this.state.loaded) {
+            butter.category.list().then((resp) => {
+                resp.data.retrieved = Date.now();
+                this.setState({
+                    loaded: true,
+                    resp: resp.data,
+                });
+                localStorage.setItem("categories", JSON.stringify(resp.data));
+            });
+        }
+    }
+
+    render() {
+        if (this.state.loaded) {
+            return (
+                <div className="grid">
+                    <Helmet>
+                        <title>Sam's TechBlog - Catégories</title>
+                    </Helmet>
+                    <Suspense fallback={<div className="loading">Loading...</div>}>
+                        <Header/>
+                        <Sidebar/>
+                    </Suspense>
+                    <div className="categories">
+                        <h1>Liste des catégories</h1>
+                        {this.state.resp.data.map((category, key) => {
+                            return (
+                                <Link to={`/blog/category/${category.slug}`} key={key}>
+                                    <div className="box">
+                                        <span>{category.name}</span>
+                                    </div>
+                                </Link>
+                            )
+                        })}
+                    </div>
                 </div>
-            </div>
-        )
+            )
+        } else {
+            return loadingDiv();
+        }
     }
 }
 
