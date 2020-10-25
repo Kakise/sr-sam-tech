3e232990c96e61e2b7d52510b1b0792c1c9d685f
Kakise <sam.taa@icloud.com>
2020-10-25 21:18:07 +0000

    Now responsive

--- src/Blog/partial/Search.js
+++ src/Blog/partial/Search.js
@@ -6,7 +6,7 @@ class SearchBar extends Component {
         return (
             <div className="search-bar">
                 <form method="get" action="/results" role="search">
-                    <input class="search" id="search" type="search" name="q" placeholder="Rechercher..." autoFocus required/>
+                    <input className="search" id="search" type="search" name="q" placeholder="Rechercher..." autoFocus required/>
                     {/*<button class="search-btn" type="submit">Go</button>-->*/}
                 </form>
             </div>
