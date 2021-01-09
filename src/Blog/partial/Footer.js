61e305da03fd0c5b11b7977fb6ec8369212b9c8a
Kakise <sam.taa@icloud.com>
2021-01-09 17:42:24 +0100

    Very minor dependencies update

--- src/Blog/partial/Footer.js
+++ src/Blog/partial/Footer.js
@@ -3,14 +3,14 @@ import "./Footer.css";
 
 class Footer extends Component {
     render() {
+        const date = new Date();
         return (
             <footer className="footer">
-                <p>Copyright © 2020 - Sami T | Made using <a href="https://buttercms.com"
-                                                             className="butter">ButterCMS</a></p>
+                <p>Copyright © 2020, {date.getFullYear()} - Sami T | Made using <a href="https://buttercms.com"
+                                                                                   className="butter">ButterCMS</a></p>
             </footer>
         );
     }
-
 }
 
 export default Footer;
